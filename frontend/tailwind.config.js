9d5c54d80e95bfe2c206ecaa09d288e882a09c00
Abhishek <abhishekkumarverma109@gmail.com>
2024-11-28 20:02:49 +0530

    add tailwindcss and modified other file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './public/index.html', // Include HTML file
+    './src/App.js', // Include all React component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
