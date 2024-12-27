f88eb283991c1a202e5433eeec90def8dcf897c9
Abhishek <abhishekkumarverma109@gmail.com>
2024-12-27 17:33:58 +0530

    Add node_modules to .gitignore

--- frontend/README.md
+++
@@ -1,7 +0,0 @@
-# HealthConnect
-
-## Objective : 
-
-Healthconnect give a better experience to connect with Doctors and Have a good consulatent for your most or first prior `Health`.
-
-### 
--- frontend/src/utilities/className.js
+++
@@ -1,5 +0,0 @@
-let color='bg-blue-50'
-let BackGroundCardColor='bg-white'
-let FirstDivClass=`flex flex-col ${ color } min-h-screen items-center justify-center`
-let CardDivClass=`${ BackGroundCardColor } w-2/5 h-2/5 rounded-lg p-6 shadow-lg text-blue-950 px-8`
-export {FirstDivClass, CardDivClass}
